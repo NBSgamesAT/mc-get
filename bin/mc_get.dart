4d40753d123e223fd58ee6e0245199a3f4925046
Nicolas Bachschwell <nicolas@wbsfilm.at>
2026-07-11 21:45:20 +0200

    Initial commit

---
+++ bin/mc_get.dart
@@ -0,0 +1,5 @@
+import 'package:mc_get/mc_get.dart' as mc_get;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mc_get.calculate()}!');
+}
---
+++ test/mc_get_test.dart
@@ -0,0 +1,8 @@
+import 'package:mc_get/mc_get.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
